a11094a49c3a2fbd1c0fc25088667c7e3da4ccee
Louie Rivas <lojayrivz@gmail.com>
2022-06-12 15:06:10 +0800

    Added Custom Auth Middleware and JWT Verify

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+    // Get token from header
+    const token = req.header('x-auth-token');
+
+    // Check if no token
+    if(!token) {
+        return res.status(401).json({
+            ms: 'No token, authorization denied'
+        });
+    }
+
+    // Verifiy token
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+        req.user = decoded.user;
+        next();
+    } catch(error) {
+        res.status(401).json({
+            msg: 'Token is not valid'
+        });
+    }
+}
