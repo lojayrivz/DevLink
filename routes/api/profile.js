4e70747cdd83d4b875750f1995af31769386403f
Louie Rivas <lojayrivz@gmail.com>
2022-06-12 22:07:46 +0800

    Added Delete Profile & User route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -151,4 +151,30 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+
+// @route   DELETE api/profile
+// @desc    Delete profile, user & posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        // @Todo - Remove Users' Post
+
+        // Remove Profile
+        await Profile.findOneAndRemove({
+            user: req.user.id
+        });
+        // Remove User
+        await User.findOneAndRemove({
+            _id: req.user.id
+        });
+
+        res.json({
+            msg: 'User deleted'
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
